a6ad2ca63410f9de5f721356ce83f20c2a820c40
adetaylor <ade@hohum.me.uk>
2021-12-10 21:49:37 -0800

    Day 11, part two but part one is v similar

---
+++ day11/src/main.rs
@@ -0,0 +1,94 @@
+use itertools::Itertools;
+
+static INPUT: &str = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+fn adjacents(x: usize, y: usize, grid: &Vec<Vec<u32>>) -> impl Iterator<Item = (usize, usize)> {
+    let w = grid[0].len();
+    let h = grid.len();
+    let mut adjacents = Vec::new();
+    if x > 0 {
+        adjacents.push((x - 1, y)); // left
+        if y > 0 {
+            adjacents.push((x - 1, y - 1)); // up & left
+        }
+        if y < h - 1 {
+            adjacents.push((x - 1, y + 1)); // down & left
+        }
+    }
+    if y > 0 {
+        adjacents.push((x, y - 1)); // up
+    }
+    if x < w - 1 {
+        adjacents.push((x + 1, y)); // right
+        if y < h - 1 {
+            adjacents.push((x + 1, y + 1)); // down and right
+        }
+        if y > 0 {
+            adjacents.push((x + 1, y - 1)); // up and right
+        }
+    }
+    if y < h - 1 {
+        adjacents.push((x, y + 1)); // down
+    }
+    adjacents.into_iter()
+}
+
+fn main() {
+    let mut grid = INPUT
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let w = grid[0].len();
+    let h = grid.len();
+    let mut total_flashes = 0usize;
+    for step in 1..=10000 {
+        let mut flashes_this_try = 0usize;
+        for cell in grid.iter_mut().map(|row| row.iter_mut()).flatten() {
+            *cell += 1;
+        }
+        let mut flashers = (0..w)
+            .cartesian_product(0..h)
+            .filter(|(x, y)| grid[*y][*x] > 9 && grid[*y][*x] != u32::MAX)
+            .collect_vec();
+        while !flashers.is_empty() {
+            for (x, y) in flashers.into_iter() {
+                flashes_this_try += 1;
+                grid[y][x] = u32::MAX;
+                for (adj_x, adj_y) in adjacents(x, y, &grid) {
+                    if grid[adj_y][adj_x] != u32::MAX {
+                        grid[adj_y][adj_x] += 1;
+                    }
+                }
+            }
+            flashers = (0..w)
+                .cartesian_product(0..h)
+                .filter(|(x, y)| grid[*y][*x] > 9 && grid[*y][*x] != u32::MAX)
+                .collect_vec();
+        }
+        for cell in grid.iter_mut().map(|row| row.iter_mut()).flatten() {
+            if *cell > 9 {
+                *cell = 0;
+            }
+        }
+        println!("Flashes on step {} = {}", step, flashes_this_try);
+        total_flashes += flashes_this_try;
+        if flashes_this_try == w*h {
+            println!("First total flash={}", step);
+            break;
+        }
+    }
+    println!("Flashes to step 100 = {}", total_flashes);
+}
